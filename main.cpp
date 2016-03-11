84b58229720ca9e5c8e085b414777103eff2c538
Antony Alexsandrov <bioqwer94@gmail.com>
2016-03-11 14:54:48 +0300

    work local but not accepted

---
+++ main.cpp
@@ -0,0 +1,51 @@
+/*
+  В начале программы мантра
+*/
+// Начало мантры
+
+#include <iostream>
+#include <cstdlib>
+#include <math.h>
+
+using namespace std;
+// Конец мантры
+
+class Time {
+public:
+    int hour;
+    int min;
+};
+
+Time add(Time a, int min) {
+
+// Здесь нужно написать код
+    Time res;
+    res.hour = a.hour;
+    res.min = a.min + min;
+
+    if (res.min > 60) {
+        int hoursAdd = (res.min / 60);
+        res.hour = res.hour + hoursAdd;
+        res.min = res.min % 60;
+    } else {
+        res.hour = a.hour;
+        res.min = a.min + min;
+    }
+    return res;
+}
+
+
+int main() {
+    Time a, newtime;
+    int min;
+
+    // Получить a  из консоли 
+
+
+    cin >> a.hour >> a.min;
+    // Получить min  из консоли
+
+    cin >> min;
+    newtime = add(a, min);
+    cout << newtime.hour << ":" << newtime.min << endl;
+}
