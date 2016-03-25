1106f1a2721dc45edef57a7c2a0f7545a007914e
Antony Alexsandrov <bioqwer94@gmail.com>
2016-03-25 15:27:36 +0300

    complex finish

--- 5.cpp
+++ 5.cpp
@@ -1,38 +1,111 @@
-// Начало мантры
+// region Mantra
 
 #include <iostream>
 #include <cstdlib>
 #include <math.h>
 
 using namespace std;
-// Конец мантры
+// endregion
 
 class C {
 public:
     double re, im;
 
-    C С() {
+    C() {
         re = 0;
         im = 0;
-        return C(0);
     }
 
-    C C(int re_) {
+    C(double re_) {
         re = re_;
         im = 0;
     }
 
-    C C(int re_, int im_) {
+    C(double re_, double im_) {
         re = re_;
         im = im_;
     }
 
-    const C operator+(C& right ){
+    C operator+(C right) {
+        return C((this->re + right.re), this->im + right.im);
+    }
 
+    C operator-(C right) {
+        return C((this->re - right.re), this->im - right.im);
+    }
+
+    friend C operator*(const C &al, const C &right) {
+        double a, b, c, d;
+        a = al.re;
+        b = al.im;
+        c = right.re;
+        d = right.im;
+        return C((a * c - b * d), (b * c + a * d));
+    }
+
+    friend C operator/(const C &al, const C &right) {
+        double a, b, c, d;
+        a = al.re;
+        b = al.im;
+        c = right.re;
+        d = right.im;
+        return C((a * c + b * d) / (c * c + d * d), (b * c - a * d) / (c * c + d * d));
+    }
+
+    bool operator==(C right) {
+        return this->re == right.re && this->im == right.im;
+    }
+
+    bool operator!=(C right) {
+        return !operator==(right);
+    }
+
+    friend std::ostream &operator<<(std::ostream &ostream1, const C &c) {
+        ostream1 << c.re << " " << c.im;
+        return ostream1;
     }
 
     double abs() {
         return sqrt(re * re + im * im);
     }
+
+    C operator+(double right) {
+        return C((this->re + right), this->im);
+    }
+
+    C operator-(double right) {
+        return C((this->re - right), this->im);
+    }
+
+    C operator*(double right) {
+        return C((this->re * right), this->im);
+    }
+
+    C operator/(int right) {
+        return C(this->re / right, this->im);
+    }
+
+    C operator+(int right) {
+        return C((this->re + right), this->im);
+    }
+
+    C operator-(int right) {
+        return C((this->re - right), this->im);
+    }
+
+    C operator*(int right) {
+        return C((this->re * right), this->im*right);
+    }
 };
+
+int main() {
+    C z(-8, 7), z2(3, 4), z3(-8, 7);
+    cout << z / z2 << endl;
+
+    C z4 = z3 / z2 + z * z2;
+    cout << z4.re << " " << z4.im << endl;//-51.84 -8.88
+
+    cout <<"deb"<<endl<< 1 / (z4 * 3 + 7); //0.00652322 0.00117007
+    return 0;
+}
 
